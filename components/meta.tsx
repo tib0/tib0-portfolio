a09f97a32bcd1559f75d77527a5a2f4d613c364a
RokinPef <thibaultmart@gmail.com>
2024-01-22 16:50:18 +0100

    Add meta keywords

--- components/meta.tsx
+++ components/meta.tsx
@@ -12,6 +12,7 @@ const Meta = () => {
         name="Folio Tib0"
         content={`Folio Tib0 réalisé avec ❤️‍🔥, 👓 and 💻.`}
       />
+      <meta name="keywords" content="Portfolio, Thibault MARTIN, Tib0, Développeur web" />
       <meta name="description" content="Portfolio de Tib0, réalisé avec ❤️‍🔥, 👓 and 💻." />
       <title>{`Folio Tib0`}</title>
       <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>" />
